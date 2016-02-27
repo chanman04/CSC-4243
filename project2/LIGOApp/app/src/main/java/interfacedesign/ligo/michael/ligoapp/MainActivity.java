a0c66531cc8b2d1153d8c88a4a6ebb642e0e53db
Michael <michaelchan985@gmail.com>
2016-02-27 02:51:28 -0600

    Added base app

---
+++ project2/LIGOApp/app/src/main/java/interfacedesign/ligo/michael/ligoapp/MainActivity.java
@@ -0,0 +1,11 @@
+package interfacedesign.ligo.michael.ligoapp;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Mike on 2/27/16.
+ */
+public class MainActivity extends ActionBarActivity{
+
+    
+}
